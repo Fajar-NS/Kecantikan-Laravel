6b5ccecc3ca130bcb9a47a76789163a88cb030f5
unknown <Fajar-NS>
2022-12-08 21:15:49 +0700

    login-regis

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -17,14 +17,33 @@
           </li>
         </ul>
         
-        <ul class="navbar-nav ms-auto text-right">          
-            <li class="nav-item">
+        <ul class="navbar-nav ms-auto">
+          @auth
+          <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Welcome back, {{ auth()->user()->name }}
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-window"></i> My Dasboard</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <form action="/logout" method="post">
+                  @csrf
+                  <button type="submit" class="dropdown-item">
+                      <i class="bi bi-box-arrow-right"></i> Logout</a>
+                  </button>
+                </form>
+              </ul>
+          </li>
+             @else
+             <li class="nav-item">
               <a href="/login" class="nav-link {{ ($active === "login") ? 'active' : '' }}">Login</a>
             </li>
             <li class="nav-item">
-                <a href="/registrasi" class="nav-link {{ ($active === "registrasi") ? 'active' : '' }}">Registrasi</a>
-            </li>
-        </ul>        
+              <a href="/register" class="nav-link {{ ($active === "registrasi") ? 'active' : '' }}">Registrasi</a>
+          </li>
+          @endauth
+      </ul>
+             
         
       </div>
     </div>
