b8ed0dd6a8d8ee3e7af6878621ae4b7c1c7cfae0
RyanfanyfadlyllahAR <fadlyllahfanny@gmail.com>
2022-12-15 08:42:56 +0700

    mengerjakan about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.main')
 
     @section('container')
-       <h1>Halaman About</h1> 
+       <h1>Next Comeric</h1> 
+       <p>PT. Next cosmetic Indonesia merupakan perusahaan jasa pembuatan kosmetik private label yang telah beroperasi sejak tahun 2011.
+          Dengan dukungan tenaga yang terampil dan profesional PT.
+          Next cosmetic Indonesia terus tumbuh dan berkembang membangun produk berkualitas dan layanan yang lebih baik.</p>
+          <p>PT. Next cosmetic Indonesia memiliki tim ahli dan profesional di bidang Desain, Penelitian dan Pengembangan
+             yang mampu untuk menyesuaikan dan memberikan inovasi produk yang sesuai dengan preferensi pelanggan. 
+             Kami berkomitmen menjalankan standar produksi kosmetik tertinggi,
+             patuh dan terakreditasi dengan persyaratan sertifikat industri (GMP/BPOM, HALAL).</p>
+
+             <p></p>
     @endsection
