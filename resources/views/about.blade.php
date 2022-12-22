52a91bf38691c0053e75984e9fa75102d64fb52e
RyanfanyfadlyllahAR <fadlyllahfanny@gmail.com>
2022-12-22 17:01:09 +0700

    peoduk

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,4 +2,20 @@
 
     @section('container')
        <h1>Halaman About</h1> 
+       <div class="card mb-3" style="max-width: 540px;">
+         <div class="row g-0">
+           <div class="col-md-4">
+             <img src="..." class="img-fluid rounded-start" alt="...">
+           </div>
+           <div class="col-md-8">
+             <div class="card-body">
+               <h5 class="card-title">Card title</h5>
+               <p class="card-text">Next Comestik adalah toko kecantikan yang memiliki produk  berkualitas terbaik yang bersertifikat BPOM. <p>Pelayanan Pelanggan:</p>
+                  09:00-23:30 jika ada produk ada yang rusak dan lain-lain, dapat ditukar/ganti baru dengan konfirmasi komplain melalui chat. Selamat berbelanja
+               </p>
+               <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+             </div>
+           </div>
+         </div>
+       </div>
     @endsection
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,5 +1,59 @@
 @extends('layouts.main')
 
     @section('container')
-       <h1>Halaman Product</h1> 
+       <h1 class="text-center mb-5">Next Cosmetik</h1> 
+       <div class="row row-cols-1 row-cols-md-2 g-4">
+         <div class="col">
+           <div class="card">
+             <img src="img/wardah.jpg" style="width: 100%; height: 300px" class="card-img-top" alt="...">
+             <div class="card-body">
+               <h5 class="card-title">Wardah Lightening Powder Foundation Light Feel</h5>
+               <p class="card-text">Bedak Yang Mencerahkan Dengan Hasil Natural</p>
+               <ol>
+                  <li>
+                     Formula yang ringan, halus dan non comedogenic, mencerahkan hingga 2 tingkat lebih cerah
+                  </li>
+                  <li>
+                     Formula yang membuat produk tetap menempel pada kulit wajah selama 12 jam
+                  </li>
+                  <li>
+                     Melindungi kulit dari sinar UV A dan UV B
+                  </li>
+               </ol>
+               <button class="btn btn-danger" type="Submit">Beli Sekarang</button>
+            </div>
+           </div>
+         </div>
+         <div class="col">
+           <div class="card">
+             <img src="img/wardah eye shadow.jpg" style="width: 100%; height: 300px" class="card-img-top" alt="...">
+             <div class="card-body">
+               <h5 class="card-title">Wardah Eye Shadow - 3 Warna</h5>
+               <p class="card-text">Sapuan warna intens untuk menonjolkan keindahan mata. Dikombinasikan dalam tiga warna sempurna untuk membantu mengekspresikan bentuk terbaik dari matamu. Terbuat dari Microcoated Particle yang nyaman dan lembut saat diaplikasikan.</p>
+               <button class="btn btn-danger" type="Submit">Beli Sekarang</button>
+            </div>
+           </div>
+         </div>
+         <div class="col">
+           <div class="card">
+             <img src="img/maybelline superstay.jpg" style="width: 100%; height: 300px" class="card-img-top" alt="...">
+             <div class="card-body">
+               <h5 class="card-title">maybelline superstay Ink</h5>
+               <p class="card-text">Maybelline Superstay Matte Ink Liquid Lipstick Stay Ink ORIGINAL 100%</p>
+               <p>Tahan seharian di bibir, Maybelline Super Stay Matte Ink ini memang punya kelebihan di daya tahannya yang mampu bertahan selama 16 jam. </p>
+               <button class="btn btn-danger" type="Submit">Beli Sekarang</button>
+             </div>
+           </div>
+         </div>
+         <div class="col">
+           <div class="card">
+             <img src="img/Make over.jpg" style="width: 100%; height: 300px" class="card-img-top" alt="...">
+             <div class="card-body">
+               <h5 class="card-title">MAKE OVER Lash Impulse Waterproof Mascara 9 ml - Mascara Waterproof & Smudgeproof</h5>
+               <p class="card-text">Make Over Lash Impulse Waterproof Mascara adalah maskara tahan air dengan 3D Maxi-Lash Technology yang menghasilkan volume 10x pada bulu mata anda menjadikannya lebih tebal, lentik, dan lebat. Maskara ini diformulasikan dengan zero-smudge formula yang menjaga performa maskara ini tidak luntur hingga 12 jam.</p>
+               <button class="btn btn-danger" type="Submit">Beli Sekarang</button>
+            </div>
+           </div>
+         </div>
+       </div>
     @endsection
